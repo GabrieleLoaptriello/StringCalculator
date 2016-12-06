a888a387281ad62303e54093bc79446241bf05fb
GabrieleLoaptriello <Gabriele@LAPTOP-5DP341KS>
2016-12-06 18:18:54 +0100

    added implementation for zero, one and two numbers

--- src/it/uniba/tdd/StringCalculator.java
+++ src/it/uniba/tdd/StringCalculator.java
@@ -2,8 +2,35 @@ package it.uniba.tdd;
 
 public class StringCalculator {
 	public int add(String numbersStr) {
-		// Returns the sum of the numbers given in numbersStr
-		// not yet implemented
-		return 0;
+		int result;
+
+		if (hasTwoNumbers(numbersStr)) {
+			result = getSum(numbersStr.split(","));
+		} else if (numbersStr.equals("")) {
+			result = 0;
+		} else {
+			result = Integer.parseInt(numbersStr);
+		}
+
+		return result;
+	}
+
+	private boolean hasTwoNumbers(String numbersStr) {
+		boolean result = false;
+		String[] numbers = numbersStr.split(",");
+
+		if (numbers.length == 2) {
+			result = true;
+		}
+
+		return result;
+	}
+
+	private String[] getNumbers(String numbersStr) {
+		return numbersStr.split(",");
+	}
+
+	private int getSum(String[] numbers) {
+		return Integer.parseInt(numbers[0]) + Integer.parseInt(numbers[1]);
 	}
 }
--- src/it/uniba/tdd/tests/StringCalculatorTest.java
+++ src/it/uniba/tdd/tests/StringCalculatorTest.java
@@ -4,11 +4,56 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import it.uniba.tdd.StringCalculator;
+
 public class StringCalculatorTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void addWillReturnZero() {
+		StringCalculator stringCalculator = new StringCalculator();
+
+		assertEquals(stringCalculator.add(""), 0);
 	}
 
+	@Test
+	public void addWillReturnOne() {
+		StringCalculator calculator = new StringCalculator();
+
+		assertEquals(calculator.add("1"), 1);
+	}
+
+	@Test
+	public void addWillReturnTwo() {
+		StringCalculator stringCalculator = new StringCalculator();
+
+		assertEquals(stringCalculator.add("2"), 2);
+	}
+
+	@Test
+	public void addWillReturnThree() {
+		StringCalculator stringCalculator = new StringCalculator();
+
+		assertEquals(stringCalculator.add("3"), 3);
+	}
+
+	@Test
+	public void addAcceptsTwoNumbers() {
+		StringCalculator stringCalculator = new StringCalculator();
+
+		assertEquals(stringCalculator.add("0,1"), 1);
+	}
+
+	@Test
+	public void addWillReturnTwoWithTwoNumbers() {
+		StringCalculator calculator = new StringCalculator();
+
+		assertEquals(calculator.add("1,1"), 2);
+	}
+
+	@Test
+	public void addWillReturnThreeWithTwoNumber() {
+		StringCalculator calculator = new StringCalculator();
+
+		assertEquals(calculator.add("1,2"), 3);
+	}
 }
